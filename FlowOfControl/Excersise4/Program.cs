b7ce4c5f3ebcc6a011810ef6508e1360105bb3e1
Leonard R <muks54321@gmail.com>
2019-08-22 13:38:42 +0300

    FlowOfControl Excersises refreshed

---
+++ FlowOfControl/Excersise4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter day number: ");
+            int dayNumber = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(PrintDayInWord(dayNumber));
+            Console.ReadLine();
+        }
+
+        static string PrintDayInWord(int n)
+        {
+            string day = "";
+                if (n <= 7)
+                {
+                    if (n == 1)
+                    {
+                        day = "Monday";
+                    }
+                    if (n == 2)
+                    {
+                        day = "Tuesday";
+                    }
+                    if (n == 3)
+                    {
+                        day = "Wednesday";
+                    }
+                    if (n == 4)
+                    {
+                        day = "Thursday";
+                    }
+                    if (n == 5)
+                    {
+                        day = "Friday";
+                    }
+                    if (n == 6)
+                    {
+                        day = "Saturday";
+                    }
+                    if (n == 7)
+                    {
+                        day = "Sunday";
+                    }
+            }
+                else
+                {
+                    day = "Not a valid day";
+            }
+
+                return day;
+        }
+    }
+}
---
+++ FlowOfControl/TenBillion/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TenBillion
+{
+    class Program
+    {
+        //TODO Write a C# program that reads an positive integer and count the number of digits the number (less than ten billion) has.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input an integer number less than ten billion: ");
+
+            var input = Convert.ToInt64(Console.ReadLine());
+            if (input < 9223372036854775807)
+            {
+                if (input.ToString().Length < 0)
+                {
+                    //todo - check if n is less than zero
+                    if (input < 0)
+                    {
+                        input *= -1;
+                    }
+                }
+
+                int digits = 1;
+                if (input > 10000000000)
+                {
+                    Console.WriteLine("Number is greater or equals 10,000,000,000!");
+                }
+                else if (input < 10000000000)
+                {
+
+                    digits = input.ToString().Length;
+
+                }
+                else
+                {
+                    Console.WriteLine("The number is not a long");
+                }
+
+                Console.WriteLine("Number of digits in the number: " + digits);
+                Console.ReadLine();
+
+
+            }
+
+        }
+    }
+}
+
