65d2644b8ef011b0f5e23eaa1cd354dd980b89d0
Leonard R <muks54321@gmail.com>
2019-08-22 13:43:24 +0300

    TypesAndVariables Excersises refreshed

---
+++ TypesAndVariables/Excersise6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write your first single digit number: ");
+            int digit1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Write your second single digit number: ");
+            int digit2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(digit1 + digit2);
+            Console.ReadLine();
+
+        }
+    }
+}
